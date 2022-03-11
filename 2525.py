bcb3a6d9960b0853354dea60907e7e7529b90099
sunghyun <81955411+sunghyun-q@users.noreply.github.com>
2022-03-11 17:30:30 +0900

    second
    24 넘어가는걸 고려

--- 2525.py
+++ 2525.py
@@ -4,8 +4,9 @@ c = int(input())
 
 h, m = divmod(b + c, 60)
 #h에 몫이 m에 나머지가 들어감 b + c를 안더하면 자릿수 넘어가는걸 고려해줘야됨
-if a + h == 24:
-    print(0, m)
 
+if a + h >= 24:
+    print(a + h - 24, m)
+    
 else:
     print(a + h, m)
